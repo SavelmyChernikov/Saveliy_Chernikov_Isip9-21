d2d3712b3c5e1aed95ea8c01316eaa2ed0e5a1b8
SavelmyChernikov <124134859+SavelmyChernikov@users.noreply.github.com>
2025-02-03 17:38:17 +0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,30 @@ document.addEventListener('DOMContentLoaded', function() {
         nav.classList.toggle('active');
     });
   });
+  
+  function animateOpacity(element, targetOpacity, duration) {
+    let start = null;
+    const startOpacity = parseFloat(element.style.opacity || 0); // Убеждаемся, что берем 0 если opacity не задано
+    targetOpacity = parseFloat(targetOpacity);
+  
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      const progress = Math.min(1, (timestamp - start) / duration);
+      element.style.opacity = (startOpacity + (targetOpacity - startOpacity) * progress);
+  
+      if (progress < 1) {
+        requestAnimationFrame(step);
+      } else {
+        element.style.opacity = targetOpacity; // Явно ставим targetOpacity
+      }
+    }
+  
+    requestAnimationFrame(step);
+  }
+  
+  const myElement = document.getElementById(' swiper-slide hero-section swiper-slide-visible swiper-slide-active  ');
+  myElement.style.opacity = 0; // Начальное значение 0
+  animateOpacity(myElement, 1, 500); // Анимация до opacity 1 за 500ms
+  element.addEventListener('transitionend', function() {
+    element.style.opacity = 1;
+});
