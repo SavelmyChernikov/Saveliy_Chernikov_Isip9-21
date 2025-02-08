23ab548f757317636a06abd0416e1f9dbea3ff16
SavelmyChernikov <124134859+SavelmyChernikov@users.noreply.github.com>
2025-02-08 16:58:55 +0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
 "use strict";
+function reveal() {
+  var reveals = document.querySelectorAll(".feature-card, .story, .services-block, .feedback-container-oc");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("show");
+    } else {
+      reveals[i].classList.remove("show");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// Проверяем видимость элементов при загрузке страницы и при прокрутке
+window.addEventListener('load', checkVisibility);
+window.addEventListener('scroll', checkVisibility);
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Working...');
